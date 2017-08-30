089fe0a92562ea9f999232d0738d63a8785f334f
Qingxin Tan <cicitan0502@gmail.com>
2017-08-30 14:23:22 +0100

    Finished Deck Class and tests

--- src/test/java/io/zipcoder/CardsTests/DeckTest.java
+++ src/test/java/io/zipcoder/CardsTests/DeckTest.java
@@ -1,7 +1,40 @@
 package io.zipcoder.CardsTests;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.zipcoder.Cards.Deck;
+
+
 /**
  * Created by W550952 on 30/08/2017.
  */
 public class DeckTest {
+	
+	@Test
+	public void creatDeskTest(){
+		//: Given
+		Deck deck = new Deck();
+		int expected = 52;
+		
+		//: When
+		int actual = deck.getDeck().size();
+		
+		//: Then
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void drawTest(){
+		//: Given
+		Deck deck = new Deck();
+		int expected = 51;
+		
+		//: When
+		deck.draw();
+		int actual = deck.getNoOfCards();
+		
+		//: Then
+		Assert.assertEquals(expected, actual);
+	}
 }

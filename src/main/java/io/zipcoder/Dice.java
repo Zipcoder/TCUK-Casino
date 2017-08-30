b515ad7de64ef3dd2a45fb477b563a161e3dbaa9
Matt Kaminski <map.kaminski@gmail.com>
2017-08-30 13:19:17 +0100

    added empty Dice classes and test

---
+++ src/main/java/io/zipcoder/Dice.java
@@ -0,0 +1,12 @@
+package io.zipcoder;
+
+public class Dice {
+
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 6;
+	
+	public static int rollDice(int numberOfDiceToRoll){
+		return 0;
+	}
+	
+}
---
+++ src/test/java/io/zipcoder/DiceTest.java
@@ -0,0 +1,13 @@
+package io.zipcoder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DiceTest {
+
+	@Test
+	public void testRollDice() {
+		Assert.fail("Not yet implemented");
+	}
+
+}

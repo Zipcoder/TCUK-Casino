16737d0bf23abac52eae3c04a3c50ccbb0882974
Matt Kaminski <map.kaminski@gmail.com>
2017-08-30 14:52:02 +0100

    added DiceGame and HigherLowerDiceGame classes

---
+++ src/main/java/io/zipcoder/Games/DiceGame.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package io.zipcoder.Games;
+
+/**
+ * @author F628559
+ *
+ */
+public abstract class DiceGame extends Game {
+	
+}
---
+++ src/main/java/io/zipcoder/Games/HigherLowerDiceGame.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package io.zipcoder.Games;
+
+/**
+ * @author F628559
+ *
+ */
+public class HigherLowerDiceGame extends DiceGame {
+
+	@Override
+	public void playGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

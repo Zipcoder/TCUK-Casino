2947045c822d7e0d19b6ea62e34046d8a10dc908
Lily Roberts <lilyroberts95@gmail.com>
2017-08-30 13:11:32 +0100

    Added card test package

--- src/main/java/io/zipcoder/Player.java
+++ src/main/java/io/zipcoder/Player.java
@@ -4,4 +4,5 @@ package io.zipcoder;
  * Created by W550952 on 30/08/2017.
  */
 public class Player {
+
 }
--- src/test/java/io/zipcoder/PlayerTest.java
+++ src/test/java/io/zipcoder/PlayerTest.java
@@ -1,11 +1,46 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by W550952 on 30/08/2017.
  */
 public class PlayerTest {
+    Player player;
+
+    @Before
+    public void setUpTests(){
+        player = new Player("Harry", 500);
+    }
+
     @Test
-    reduceBalanceTest
+    public void reduceBalanceTest(){
+        boolean condition = player.reduceBalance(250);
+
+        Assert.assertTrue(condition);
+    }
+
+    @Test
+    public void increaseBalanceTest(){
+        boolean condition = player.increaseBalance(250);
+
+        Assert.assertTrue(condition);
+    }
+
+    @Test
+    public void reduceBalanceTest(){
+        boolean condition = player.reduceBalance(-250);
+
+        Assert.assertFalse(condition);
+    }
+
+    @Test
+    public void increaseBalanceTest(){
+        boolean condition = player.increaseBalance(-250);
+
+        Assert.assertFalse(condition);
+    }
+
 }

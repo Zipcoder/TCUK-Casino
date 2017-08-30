eba272142701023fd24f26c232a94bf43a0f06e0
Matt Kaminski <map.kaminski@gmail.com>
2017-08-30 13:57:18 +0100

    implemented the bettable interface, added javadoc to Dice

--- src/main/java/io/zipcoder/Dice.java
+++ src/main/java/io/zipcoder/Dice.java
@@ -7,13 +7,20 @@ public class Dice {
 	private static final int MIN_NUMBER = 1;
 	private static final int MAX_NUMBER = 6;
 	private static final Random randGen = new Random();
-	
-	public static int rollDice(int numberOfDiceToRoll){
+
+	/**
+	 * Returns an integer value between 1 and the number of dice to roll
+	 * multiplied by 6.
+	 * 
+	 * @param numberOfDiceToRoll
+	 * @return sum value of dice rolls
+	 */
+	public static int rollDice(int numberOfDiceToRoll) {
 		int result = 0;
-		for (int i = 0; i<numberOfDiceToRoll; i++){
+		for (int i = 0; i < numberOfDiceToRoll; i++) {
 			result += randGen.nextInt(MAX_NUMBER) + 1;
 		}
 		return result;
 	}
-	
+
 }
--- src/test/java/io/zipcoder/DiceTest.java
+++ src/test/java/io/zipcoder/DiceTest.java
@@ -7,18 +7,29 @@ public class DiceTest {
 
 	@Test
 	public void testRollDice() {
-		
+
 		// :Given
-		
+
 		int expectedMaxBound = 6;
 		int expectedMinBound = 1;
 		int numberOfDice = 1;
-		
+
 		// :When
-		for (int toRun = 0; toRun<1000; toRun++){
+		for (int toRun = 0; toRun < 1000; toRun++) {
 			int tempNum = Dice.rollDice(1);
-			if (!(1<=tempNum && tempNum<=6)){
-				// :Then -- only triggered if number is not between 1 to 6 inclusive.
+			if (!(1 <= tempNum && tempNum <= 6)) {
+				// :Then -- only triggered if number is not between 1 to 6
+				// inclusive.
+				Assert.fail("testRollDice - number of dice = 1; number returned not within range of 1 to 6");
+			}
+		}
+
+		// :When
+		for ( int toRun = 0; toRun < 10000; toRun++) {
+			int tempNum = Dice.rollDice(5);
+			if (!(1 <= tempNum && tempNum <= 6*5)) {
+				// :Then -- only triggered if number is not between 1 to 30
+				// inclusive.
 				Assert.fail("testRollDice - number of dice = 1; number returned not within range of 1 to 6");
 			}
 		}

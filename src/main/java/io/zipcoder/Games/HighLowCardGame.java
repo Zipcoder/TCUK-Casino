35b16731710c6a337a6e35d450fb3a473ca6d920
Olive Hopkin <olivehopkin@icloud.com>
2017-08-30 15:39:17 +0100

    Small formatting edits

--- src/main/java/io/zipcoder/Games/HighLowCardGame.java
+++ src/main/java/io/zipcoder/Games/HighLowCardGame.java
@@ -2,8 +2,5 @@ package io.zipcoder.Games;
 
 public class HighLowCardGame {
 
-	public HighLowCardGame() {
-		// TODO Auto-generated constructor stub
-	}
 
 }
--- src/test/java/io/zipcoder/HandlersTests/HandlerTest.java
+++ src/test/java/io/zipcoder/HandlersTests/HandlerTest.java
@@ -1,10 +1,5 @@
 package io.zipcoder.HandlersTests;
 
-import io.zipcoder.Cards.Card;
-import io.zipcoder.Cards.Hand;
-import io.zipcoder.Cards.Suit;
-import io.zipcoder.Cards.Value;
-
 public class HandlerTest {
 
 

ce255dce2daaccfc937e09dc858657ce953c7ffc
Olive Hopkin <olivehopkin@icloud.com>
2017-08-30 14:26:12 +0100

    Card toString test written and passed

--- src/test/java/io/zipcoder/CardsTests/CardTest.java
+++ src/test/java/io/zipcoder/CardsTests/CardTest.java
@@ -1,7 +1,30 @@
 package io.zipcoder.CardsTests;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.zipcoder.Cards.Suit;
+import io.zipcoder.Cards.Value;
+import io.zipcoder.Cards.*;
+
+
 /**
  * Created by W550952 on 30/08/2017.
  */
 public class CardTest {
+
+	// Test toString method
+
+	@Test
+	public void toStringTest() {
+		// : Given
+		Card card = new Card(Suit.Clubs, Value.TWO);
+		String expected = "TWO of Clubs";
+		// : When
+		String actual = card.toString();
+		// : Then
+		Assert.assertEquals(expected, actual);
+		
+	}
 }

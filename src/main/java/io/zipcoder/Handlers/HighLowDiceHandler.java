d8bd106e5e7435b22bb72a73ca7387947309afe9
Olive Hopkin <olivehopkin@icloud.com>
2017-08-31 13:27:06 +0100

    Addition of methods to HighLowDiceHandler

--- src/main/java/io/zipcoder/Handlers/HighLowDiceHandler.java
+++ src/main/java/io/zipcoder/Handlers/HighLowDiceHandler.java
@@ -1,22 +1,25 @@
 package io.zipcoder.Handlers;
 
+import io.zipcoder.Bettable;
 import io.zipcoder.Dice;
 import io.zipcoder.Player;
 
-public class HighLowDiceHandler extends DiceHandler {
+public class HighLowDiceHandler extends DiceHandler implements Bettable {
 
 	private int diceValue;
 	private int numberOfDice;
 	private Player player;
 
+	//constructor
 	public HighLowDiceHandler(Player player) {
 		this.player = player;
 	}
-
+	
+	//get player
 	public Player getPlayer() {
 		return player;
 	}
-
+	
 	public void setDiceValue(int numberOfDice) {
 		this.numberOfDice = numberOfDice;
 		this.diceValue = Dice.rollDice(numberOfDice);
@@ -30,5 +33,21 @@ public class HighLowDiceHandler extends DiceHandler {
 	public int getNumberOfDice() {
 		return numberOfDice;
 	}
+	
+	//wrapper methods - create a method that only allows us to give/take an amount 
+	public Player giveMoney(Player player, double amountToAdd) {
+	
+		// TODO Auto-generated method stub
+		return null;
+		
+	}
 
+	public Player takeMoney(Player player, double amountToRemove) {
+		// TODO Auto-generated method stub
+		//check available funds first is > 0 - getBalance method on player
+		//
+		return null;
+	}
+	
+	
 }

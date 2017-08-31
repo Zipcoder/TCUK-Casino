7032935fbef475a3f4011c909850c7bfd2a5f2db
Olive Hopkin <olivehopkin@icloud.com>
2017-08-31 11:36:24 +0100

    constructor added to h/LDiceHandler

--- src/main/java/io/zipcoder/Handlers/HighLowDiceHandler.java
+++ src/main/java/io/zipcoder/Handlers/HighLowDiceHandler.java
@@ -1,30 +1,34 @@
 package io.zipcoder.Handlers;
+
 import io.zipcoder.Dice;
+import io.zipcoder.Player;
 
 public class HighLowDiceHandler extends DiceHandler {
-	
+
 	private int diceValue;
 	private int numberOfDice;
-	
-	
-	
+	private Player player;
+
+	public HighLowDiceHandler(Player player) {
+		this.player = player;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
 	public void setDiceValue(int numberOfDice) {
-		
 		this.numberOfDice = numberOfDice;
 		this.diceValue = Dice.rollDice(numberOfDice);
-		
 	}
 
 	public int getDiceValue() {
-		//get value from rollDice function 
+		// get value from rollDice function
 		return diceValue;
-		
 	}
-	
+
 	public int getNumberOfDice() {
-		
 		return numberOfDice;
-		
 	}
 
 }

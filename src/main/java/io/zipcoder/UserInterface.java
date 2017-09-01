323ea29470018aa1801b0273de37d0d4e206aa60
Matt Kaminski <map.kaminski@gmail.com>
2017-09-01 14:41:28 +0100

    added bug fixes to user interface, and barebone help function

--- src/main/java/io/zipcoder/UserInterface.java
+++ src/main/java/io/zipcoder/UserInterface.java
@@ -1,38 +1,73 @@
 package io.zipcoder;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * Created by W550952 on 30/08/2017.
  */
 public class UserInterface {
+
 	public static Scanner sc = new Scanner(System.in);
-    public static String getUserInputString(){
-        //Scanner sc = new Scanner(System.in);
-        String output = sc.nextLine();
-        //sc.close();
-        return output;
-    }
 
-    public static int getUserInput(){
-        //Scanner sc = new Scanner(System.in);
-        int output = sc.nextInt();
-        // clear buffer
-        sc.nextLine();
-        //sc.close();
-        return output;
-    }
-    
-    public static Double getUserInputDouble(){
-        //Scanner sc = new Scanner(System.in);
-        double output = sc.nextDouble();
-     // clear buffer
-        sc.nextLine();
-        //sc.close();
-        return output;
-    }
+	public static String getUserInputString() {
 
-    public String welcome(){
-        return "Welcome to the Casino!";
-    }
+		String output = sc.nextLine();
+
+		if (UserInterface.checkIfInputHas(output, "help")) {
+			help();
+		}
+
+		return output;
+	}
+
+	public static int getUserInput() {
+
+		boolean condition = true;
+		int output = 0;
+		while (condition) {
+			String intString = getUserInputString();
+			try{
+				output = Integer.parseInt(intString);
+				condition = false;
+			} catch (Exception e) {
+				System.out.println("Sorry, could you input an integer?");
+			}
+		}
+
+		sc.nextLine();
+		// sc.close();
+		return output;
+	}
+
+	public static Double getUserInputDouble() {
+
+		boolean condition = true;
+		double output = 0;
+		while (condition) {
+			String doubleString = getUserInputString();
+			try{
+				output = Double.parseDouble(doubleString);
+				condition = false;
+			} catch (Exception e) {
+				System.out.println("Sorry, could you input a double?");
+			}
+		}
+
+		sc.nextLine();
+
+		return output;
+	}
+
+	public String welcome() {
+		return "Welcome to the Casino!";
+	}
+
+	public static boolean checkIfInputHas(String inputString, String whatToCheckItContains) {
+		return inputString.toLowerCase().indexOf(whatToCheckItContains) != -1;
+	}
+
+	private static void help() {
+		System.out.println("help triggered");
+	}
 }

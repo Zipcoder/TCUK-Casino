637516e65a2305eac3da04b5cf34631a8fe272cd
Matt Kaminski <map.kaminski@gmail.com>
2017-09-01 15:58:55 +0100

    added overloaded functionality into UserInputClass

--- src/main/java/io/zipcoder/UserInterface.java
+++ src/main/java/io/zipcoder/UserInterface.java
@@ -8,7 +8,21 @@ import java.util.regex.Pattern;
  */
 public class UserInterface {
 
-	public static Scanner sc = new Scanner(System.in);
+	private static Scanner sc = new Scanner(System.in);
+	private static boolean useSystemOutFunciton = true;
+
+	/**
+	 * Show the inputed userReadableString to the user and then receive an input
+	 * from the user as a string
+	 * 
+	 * @param userReadableString
+	 * @return string input from the user
+	 */
+	public static String getUserInputString(String userReadableString) {
+		if (useSystemOutFunciton)
+			sendUpwardsToUser(userReadableString);
+		return getUserInputString();
+	}
 
 	public static String getUserInputString() {
 
@@ -20,9 +34,18 @@ public class UserInterface {
 
 		return output;
 	}
-	
-	public static double stripOtherCharsFromExpectedNumericString(){
-		return 0.0;
+
+	/**
+	 * Show the inputed userReadableString to the user and then receive an input
+	 * from the user as an integer
+	 * 
+	 * @param userReadableString
+	 * @return int user input
+	 */
+	public static int getUserInput(String userReadableString) {
+		if (useSystemOutFunciton)
+			sendUpwardsToUser(userReadableString);
+		return getUserInput();
 	}
 
 	public static int getUserInput() {
@@ -31,14 +54,27 @@ public class UserInterface {
 		int output = 0;
 		while (condition) {
 			String intString = getUserInputString();
-			try{
+			try {
 				output = Integer.parseInt(intString);
 				condition = false;
 			} catch (Exception e) {
-				System.out.println("Sorry, could you input an integer?");
+				sendUpwardsToUser("Sorry, could you input an integer?");
 			}
 		}
 		return output;
+	}
+
+	/**
+	 * Show the inputed userReadableString to the user and then receive an input
+	 * from the user as a Double (can be auto-unboxed to double)
+	 * 
+	 * @param userReadableString
+	 * @return Double user input
+	 */
+	public static Double getUserInputDouble(String userReadableString) {
+		if (useSystemOutFunciton)
+			sendUpwardsToUser(userReadableString);
+		return getUserInputDouble();
 	}
 
 	public static Double getUserInputDouble() {
@@ -47,11 +83,11 @@ public class UserInterface {
 		double output = 0;
 		while (condition) {
 			String doubleString = getUserInputString();
-			try{
+			try {
 				output = Double.parseDouble(doubleString);
 				condition = false;
 			} catch (Exception e) {
-				System.out.println("Sorry, could you input a double?");
+				sendUpwardsToUser("Sorry, could you input a double?");
 			}
 		}
 
@@ -62,6 +98,13 @@ public class UserInterface {
 		return "Welcome to the Casino!";
 	}
 
+	/**
+	 * checks if "whatToCheckItContains" is present in the "inputString"
+	 * 
+	 * @param inputString
+	 * @param whatToCheckItContains
+	 * @return boolean true if the input string is contained in the whatToCheckItContains string
+	 */
 	public static boolean checkIfInputHas(String inputString, String whatToCheckItContains) {
 		return inputString.toLowerCase().indexOf(whatToCheckItContains) != -1;
 	}
@@ -69,4 +112,17 @@ public class UserInterface {
 	private static void help() {
 		System.out.println("help triggered");
 	}
+
+	private static double stripOtherCharsFromExpectedNumericString() {
+		return 0.0;
+	}
+
+	private static void sendUpwardsToUser(String userReadableString) {
+		if (useSystemOutFunciton) {
+			System.out.println(userReadableString);
+		} else {
+			throw new RuntimeException("Feature yet to be implemented");
+		}
+	}
+
 }
